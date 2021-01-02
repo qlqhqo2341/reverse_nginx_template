10aa0d2bd3f4b4dd5ad0b18d8fe7b325b8d479c8
jeonghan lee <qlqhqo2341@naver.com>
2021-01-02 17:07:13 +0900

    code-server 연결 포인트 추가 및 htpasswd 폴더 연결

--- restart.sh
+++ restart.sh
@@ -3,6 +3,7 @@
 
 NGINX_CONFIG='/home/pi/nginx/nginx_config/nginx.conf'
 NGINX_CONFIG_CONFD='/home/pi/nginx/nginx_config/conf.d'
+NGINX_CONFIG_HTPASSWD='/home/pi/nginx/nginx_config/htpasswd'
 CONTAINER_NAME='nginx'
 DOCKER_PS_CONTAINER_CNT=`docker ps -a | awk '{print $NF}' | grep ${CONTAINER_NAME} | wc -l`
 
@@ -13,8 +14,10 @@ fi;
 
 echo 'nginx container create'
 docker run -dt -p 80:80 -p 443:443 --name ${CONTAINER_NAME} \
+--restart unless-stopped \
 -v ${NGINX_CONFIG}:/etc/nginx/nginx.conf:ro \
 -v ${NGINX_CONFIG_CONFD}:/etc/nginx/conf.d:ro \
+-v ${NGINX_CONFIG_HTPASSWD}:/etc/nginx/htpasswd:ro \
 -v /var/log/nginx/:/var/log/nginx/ \
 -v /var/cache/jenkins/war:/var/cache/jenkins/war:ro \
 -v /var/lib/jenkins/:/var/lib/jenkins/:ro \
