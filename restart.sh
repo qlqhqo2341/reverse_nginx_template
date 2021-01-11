eb231c2f74533fbf093119181a3ef7ddabed681e
jeonghan lee <qlqhqo2341@naver.com>
2021-01-12 00:58:32 +0900

    htpasswd 추가 (미사용) 및 pi로 code-server 변경

--- restart.sh
+++ restart.sh
@@ -3,7 +3,8 @@
 
 NGINX_CONFIG='/home/pi/nginx/nginx_config/nginx.conf'
 NGINX_CONFIG_CONFD='/home/pi/nginx/nginx_config/conf.d'
-NGINX_CONFIG_HTPASSWD='/home/pi/nginx/nginx_config/htpasswd'
+NGINX_CONFIG_HTPASSWD='/home/pi/nginx/nginx_config/.htpasswd'
+NGINX_WEBDAV='/home/pi/webdav'
 CONTAINER_NAME='nginx'
 DOCKER_PS_CONTAINER_CNT=`docker ps -a | awk '{print $NF}' | grep ${CONTAINER_NAME} | wc -l`
 
@@ -17,11 +18,12 @@ docker run -dt -p 80:80 -p 443:443 --name ${CONTAINER_NAME} \
 --restart unless-stopped \
 -v ${NGINX_CONFIG}:/etc/nginx/nginx.conf:ro \
 -v ${NGINX_CONFIG_CONFD}:/etc/nginx/conf.d:ro \
--v ${NGINX_CONFIG_HTPASSWD}:/etc/nginx/htpasswd:ro \
+-v ${NGINX_CONFIG_HTPASSWD}:/etc/nginx/.htpasswd:ro \
 -v /var/log/nginx/:/var/log/nginx/ \
 -v /var/cache/jenkins/war:/var/cache/jenkins/war:ro \
 -v /var/lib/jenkins/:/var/lib/jenkins/:ro \
 -v /etc/letsencrypt:/etc/letsencrypt:ro \
+-v ${NGINX_WEBDAV}:/webdav \
 -e TZ='Asia/Seoul' \
 nginx:alpine
 
